844855c39251f94f9966b3ed3be9bcad8bb855ea
maura22 <maurastph@gmail.com>
2025-08-07 16:49:53 +0100

    Staff-Attendance-Group3, funtionality inproved

--- javasript/validation.js
+++ javasript/validation.js
@@ -1,9 +1,31 @@
+/*
+let db;
 
+const request = indexedDB.open("WorkForceDB", 1);
+request.onsuccess =  (e) =>{
+  alert("Database opened successfully!" );
+};
+
+request.onupgradeneeded =  (event) => {
+
+ console.log("getData.......12345");
+  db = event.target.result;
+  var id = 1;
+const tx = db.createObjectStore("staff", { keyPath: "status", autoIncrement: true });
+const store =db.transaction("staff","readwrite");
+console.log(staff);
+store.put({staff});
+
+};
+*/
+
+
+ var staffArray = [];
 
 
 function validity(){
     event.preventDefault();
-  
+
     if(document.getElementById("fname").value=="")
   {
     document.getElementById("fname-respons").classList.add('invalid-feedback1')
@@ -12,59 +34,45 @@ function validity(){
     document.getElementById("fname-respons").classList.add('valid-feedback1')
     document.getElementById("fname-respons").innerHTML="valid"
   }
- getData();
- registerStaff()
+
+var staff = getData();
+
+staffArray.push(staff);
+console.log(staffArray);
+alert("Staff sucessfully saved");
+saveData();
+document.getElementById("verify").reset();
+console.log(JSON.parse(localStorage.getItem("staffArray")));
 }
+
+
 function getData(){
+  var staff = {};
   const name1 = document.getElementById('fname').value;
   const name2 = document.getElementById('lastname').value;
   const dateOfBirth = document.getElementById('dateBirth').value;
   const placeOfBirth = document.getElementById('placeBirth').value;
-  const selectedGender = document.querySelector('input[name="gender"]:checked');
+  const selectedGender = document.querySelector('input[name="gender"]:checked').value;
+  const status =document.getElementById('status').value;
   
+  
+  staff.fistname = name1;
+  staff.dateOfBirth=dateOfBirth;
+  staff.department = department.value;
+  staff.selectedGender=selectedGender;
+  staff.lastName=name2;
+  staff.staff_status=status;
+  return staff;
+}
+   
 
-  const Status =  document.getElementById('status').value;
+function saveData() {
+
+   localStorage.setItem("staffArray",JSON.stringify(staffArray));
 
 }
-function handleChange(selectObj) {
-  const value = selectObj.value;
-  console.log(value);
-}
-
-if (!window.indexedDB) {
-  console.log("Your browser doesn't support IndexedDB.");
-} else {
-  alert('succes!')
-}
-let db;
-
-const request = indexedDB.open("WorkForceDB", 1);
-
-request.onerror = function () {
- alert("Database failed to open");
-};
-
-request.onsuccess = function () {
-  db = request.result;
-  alert("Database opened successfully!");
-};
-
-request.onupgradeneeded = function (e) {
-  db = e.target.result;
-
-  const store = db.createObjectStore("staff", { keyPath: "id", autoIncrement: true });
-
-  store.createIndex("firstName", "firstName", { unique: false });
-  store.createIndex("lastName", "lastName", { unique: false });
-  store.createIndex("dateOfBirth", "dateOfBirth", { unique: false });
-  store.createIndex("placeOfBirth", "placeOfBirth" ,{unique: false});
-  store.createIndex("gender", "gender" ,{unique: false});
-  store.createIndex("department", "department", { unique: false });
-  store.createIndex("status", "status", { unique: false });
-};
-
-function registerStaff() {
-  getData()
+/*function registerStaff() {
+  
   handleChange(selectObj) 
   const staffData = {
     firstName:"vivtor",
@@ -74,12 +82,5 @@ function registerStaff() {
     department:"IT",
     status: "temporal"
   };
-
-  const transaction = db.transaction(["staff"], "readwrite");
-  const store = transaction.objectStore("staff");
-  const request = store.add(staffData);
-
-  request.onsuccess = () => alert("Staff registered successfully!");
-  request.onerror = () => alert("Failed to register staff.");
-}
+*/
 
